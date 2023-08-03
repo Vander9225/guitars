bef741ad9fe0a5882c3aada3c0bb1d2f32384773
Vander9225 <vanserkons@gmail.com>
2023-08-03 15:30:53 +0300

    last

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -1,4 +1,4 @@
-import React, { useContext} from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../AppContext';
 import { Rating } from '@mui/material';
 import { useState, useEffect } from 'react';
@@ -21,23 +21,33 @@ const CommentItem = ({commentary}) => {
     },[voteComment])
 
     const incrementVote = () => {
-        setVoteComment((prevState) => vote + 1);
+        setVoteComment(vote + 1);
         setVoteUp(!voteUp);
         setVoteDown(false)
+        console.log(vote, voteComment)
+        if(voteUp){
+            setVoteComment(vote)
+        }
     }
 
     const decrementVote = () => {
-        setVoteComment((prevState) => vote - 1);
+        setVoteComment(vote - 1);
         setVoteDown(!voteDown)
         setVoteUp(false)
+        if(voteDown){
+            setVoteComment(vote)
+        }
+
     }
+
+
 
     return (
     <div className='comment-box'>
         <div className='rate-box'>
           <Rating className="comment-rate" name="read-only" value={rate} readOnly />
           <p className='rate-vote'>
-            <button onClick={incrementVote} className='vote-direction' disabled={voteUp}>▲</button>{voteComment}<button onClick={decrementVote} className='vote-direction' disabled={voteDown}>▼</button></p>
+            <button onClick={incrementVote} style={{ color: voteUp ? 'black' : 'rgb(215, 214, 214)' }} className='vote-direction'>▲</button>{voteComment}<button onClick={decrementVote} style={{ color: voteDown ? 'black' : 'rgb(215, 214, 214)' }} className='vote-direction'>▼</button></p>
         </div>
         <div  className="comment">
             <p className='comment-text'>{email.split("@")[0].charAt(0).toUpperCase() + email.split("@")[0].slice(1)}</p>
